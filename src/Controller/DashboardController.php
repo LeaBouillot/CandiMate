76bfc0e7daac8595abfa88f5802754cbf7bff4d3
bestdevelope <leabouillot7@gmail.com>
2025-01-18 16:46:51 +0100

    update

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class DashboardController extends AbstractController
+final class DashboardController extends AbstractController
 {
     #[Route('/dashboard', name: 'app_dashboard')]
     public function index(): Response
