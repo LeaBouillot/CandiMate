11923548e5c95ca534c0ca9d9606541b6a2b4f98
bestdevelope <leabouillot7@gmail.com>
2025-01-19 17:30:44 +0100

    update

--- src/Controller/JobOfferController.php
+++ src/Controller/JobOfferController.php
@@ -4,16 +4,15 @@ namespace App\Controller;
 
 use App\Repository\JobOfferRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class JobOfferController extends AbstractController
 {
     #[Route('/job-offers', name: 'app_job_offer_all', methods: 'GET')]
-    public function all(JobOfferRepository $jor): Response
+    public function all(JobOfferRepository $jr): Response
     {
-        $jobs = $jor->findBy(['app_user' => $this->getUser()]);
-        return $this->render('job_offer/list.html.twig', ['jobs' => '$jobs']);
+        $jobOffers = $jr->findBy(['app_user' => $this->getUser()]);
+        return $this->render('job_offer/list.html.twig', ['job_offers' => $jobOffers,]);
     }
 };
