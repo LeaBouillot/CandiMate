f61dbf5a8124f5e9ea76d53821b6e8af84ec7980
bestdevelope <leabouillot7@gmail.com>
2025-02-05 19:53:54 +0100

    profile

---
+++ src/Controller/ProfileController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Form\ProfileType;
+use Symfony\Component\String\Slugger\SluggerInterface;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Doctrine\ORM\EntityManagerInterface;
+
+#[Route('/profile')]
+#[IsGranted('ROLE_USER')]
+#[IsGranted('IS_AUTHENTICATED_FULLY')]
+class ProfileController extends AbstractController
+{
+   #[Route('', name: 'app_profile_show', methods: ['GET'])]
+   public function show(): Response
+   {
+       return $this->render('profile/show.html.twig', [
+           'user' => $this->getUser()
+       ]);
+   }
+
+   #[Route('/edit', name: 'app_profile_edit', methods: ['GET', 'POST'])]
+   public function edit(Request $request, EntityManagerInterface $entityManager): Response
+   {
+       $user = $this->getUser();
+       $form = $this->createForm(ProfileType::class, $user);
+       $form->handleRequest($request);
+
+       if ($form->isSubmitted() && $form->isValid()) {
+           $imageFile = $form->get('imageFile')->getData();
+           
+           if ($imageFile) {
+               $originalFilename = pathinfo($imageFile->getClientOriginalName(), PATHINFO_FILENAME);
+               $newFilename = $originalFilename.'-'.uniqid().'.'.$imageFile->guessExtension();
+
+               try {
+                   $imageFile->move(
+                       $this->getParameter('profile_images_directory'),
+                       $newFilename
+                   );
+                   $user->setImage($newFilename);
+               } catch (\Exception $e) {
+                   $this->addFlash('error', 'Error uploading image');
+                   return $this->redirectToRoute('app_profile_edit');
+               }
+           }
+
+           $entityManager->flush();
+           $this->addFlash('success', 'Profile updated successfully');
+           return $this->redirectToRoute('app_profile_show');
+       }
+
+       return $this->render('profile/edit.html.twig', [
+           'form' => $form->createView(),
+       ]);
+   }
+}
---
+++ src/Form/ProfileType.php
@@ -0,0 +1,51 @@
+<?php
+
+// src/Form/ProfileType.php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\File;
+
+class ProfileType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('firstName', TextType::class, [
+                'label' => 'Prénom'
+            ])
+            ->add('lastName', TextType::class, [
+                'label' => 'Nom'
+            ])
+            ->add('email', EmailType::class)
+            ->add('imageFile', FileType::class, [
+                'label' => 'Photo de profil',
+                'mapped' => false,
+                'required' => false,
+                'constraints' => [
+                    new File([
+                        'maxSize' => '1024k',
+                        'mimeTypes' => [
+                            'image/jpeg',
+                            'image/png',
+                        ],
+                        'mimeTypesMessage' => 'Please upload a valid image file',
+                    ])
+                ],
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
